5b554687213a665500a4365f5b6d4bf1b3d5c2f0
Nitish Sahu <sahunitish2005@gmail.com>
2026-06-24 15:32:02 +0530

    Time: 779 ms (5%), Space: 20.6 MB (83.24%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int count = 0, num = -1; 
+        for(int i = 0; i < nums.size(); i++){
+            count = 0; 
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[i] == nums[j]) count++; 
+            }
+            if(count == 1) num = nums[i]; 
+        }
+
+        return num; 
+    }
+
+    // int singleNumber(vector<int>& nums) {
+    //     map<long long, int> mp; 
+
+    //     for(int i = 0; i < nums.size(); i++){
+    //         mp[nums[i]]++;
+    //     }
+
+    //     for(auto it: mp){
+    //         if(it.second == 1){
+    //             return it.first; 
+    //         }
+    //     }
+
+    //     return -1; 
+    // }
+};
