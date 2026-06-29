8c485210f1b8dcb8efc8514ba4ea5f6bb2ca46da
Nitish Sahu <sahunitish2005@gmail.com>
2026-06-29 20:33:15 +0530

    Time: 4 ms (18.43%), Space: 28.3 MB (29.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> mpp; 
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++; 
+        int count = 0, el;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+               el = nums[i];
+               count = 1; 
+            }
+            else if(el == nums[i]) count++; 
+            else count--; 
         }
 
-        for(auto it : mpp){
-            if(it.second > nums.size() / 2) return it.first; 
-        }
-
-        return -1;
+        return el; 
     }
 };
+
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
-//         int count = 0; 
+//         map<int, int> mpp;
 //         for(int i = 0; i < nums.size(); i++){
-//             count = 0; 
-//             for(int j = 0; j < nums.size(); j++){
-//                 if(nums[i] == nums[j]) count++; 
-//             }
-
-//             if(count > nums.size() / 2) return nums[i]; 
+//             mpp[nums[i]]++;
 //         }
 
-//         return -1; 
+//         for(auto it : mpp){
+//             if(it.second > nums.size() / 2) return it.first;
+//         }
+
+//         return -1;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int count = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             count = 0;
+//             for(int j = 0; j < nums.size(); j++){
+//                 if(nums[i] == nums[j]) count++;
+//             }
+
+//             if(count > nums.size() / 2) return nums[i];
+//         }
+
+//         return -1;
 //     }
 // };
